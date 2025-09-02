0bbfaaec9de39e00d2ccdc545c297cde4d753db4
Rich Freedman <richfreedman@gmail.com>
2025-09-02 00:17:31 -0400

    freq and signal strength from radio, mode todo

--- py-webview.py
+++ py-webview.py
@@ -8,113 +8,75 @@ from datetime import datetime
 import random
 import re  
 import radio
+from queue import Queue
 
 def getMode(): 
      # TODO: use the HamLib rigctl api to get the mode from the radio
      return 'USB'
 
-def getFreq():
-    # TODO: use the HamLib rigctl api to get the VFO frequency from the radio
-    freqHz = 7427910 # frequency in Hz
+def gaugeValueToSLabel(gaugeValue):
+    floatValue = float(gaugeValue)
+    if floatValue <= 9:
+        return f"S{floatValue}"
+    
+    return f"S9+{round(((floatValue - 9) * 10))}"
 
+def updateSMeter(jsWindow, gaugeValue):
+    cmd = f'acceptSMeter("{gaugeValue}");'
+    jsWindow.run_js(cmd) 
+
+    sLabel = gaugeValueToSLabel(gaugeValue)
+    cmd = f'acceptSValue("{sLabel}");' 
+    jsWindow.run_js(cmd)  
+
+def formatFreq(freqHz):
     # convert to MHz
-    freqMHz = freqHz / 1000000
+    freqMHz = int(freqHz) / 1000000
 
     # format to text, such that, e.g. 742791Hz becomes '7.427.910' instead of '7.42791'
     freqStr = f"{freqMHz}0"
     return freqStr[:-3] + "." + freqStr[-3:]
 
-  
-def getSignalStrengthFromRadio(): # returns signal strength in dB, generally in the range of -54dB (S0) to 60db (S9+60)
-        # TODO: use the HamLib rigctl api to get the signal strength from the radio
-        return random.uniform(-54, 60)  
-
-def getSignalStrength():
-        sLevelDb = getSignalStrengthFromRadio()
-        sLevel = strengthToSLevel(sLevelDb)
-        return round(sLevel, 1)
-
-'''
-Convert signal strength from dBm relative to S9 to range of 0-15 for the (linear) S-meter gauge
-according to https://hamlib-developer.narkive.com/UNmwDxca/icom-rig-level-strength:
-
-* RIG_LEVEL_STRENGTH: val is an integer, representing the S Meter
-* level in dB relative to S9, according to the ideal S Meter scale.
-*
-* The ideal S Meter scale is as follows: 
-* S0 =   -54, 
-* S1 =   -48, 
-* S2 =   -42, 
-* S3 =   -36,
-* S4 =   -30, 
-* S5 =   -24, 
-* S6 =   -18, 
-* S7 =   -12, 
-* S8 =    -6, 
-* S9 =     0, 
-+ S9+10 = 10, 
-* S9+20 = 20,
-* S9+30 = 30, 
-* S9+40 = 40, 
-* S9+50 = 50,
-* S9+60 = 60. 
-'''
-def strengthToSLevel(strength):
-    # under-range (< -54dBm) - return S0
-    if strength < -54:
-      print(f'strength under-range: strength {strength} is < -54, returning 0 (s0)');
-      return 0;
-
-    # over-range: > 60dBm, return S9+60
-    if strength > 60:
-        print(f'signal strength over-range: strength {strength} is > 60dBm, returning 15 (s9+60)');
-        return 15 # don't go beyond full scale at S9+60    
-
-    # S0 (-54dBm) to S9 (0dBm) - scaled from 0 to 9 for the gauge
-    if strength <= 0 :
-      return (strength + 54) / 6
-
-    # S9+ : 1dBm (S9+1), up to 60dBm (S9+60), scaled to 10 to 15 for the gauge
-    if strength > 0:
-      return (strength / 10) + 9 
-
-
-def updateSMeter(jsWindow):
-    val = getSignalStrength();
-    cmd = f'acceptSMeter("{val}");'
-    jsWindow.run_js(cmd)    
-
-def updateFreq(jsWindow):
-    val = getFreq()
-    cmd = f'acceptVFO("{val}");'
+def updateFreq(jsWindow, freq):
+    formatted = formatFreq(freq)
+    cmd = f'acceptVFO("{formatted}");'
     jsWindow.run_js(cmd) 
 
-def updateMode(jsWindow):
-    val = getMode()
-    cmd = f'acceptMode("{val}")'
-    jsWindow.run_js(cmd) 
 
 ## the python background thread:
 ## talk to hamlib to get the radio data and send the result to the webview, once every 1/2 second
 # TODO: when actually using rigCtl to talk to the radio, it will be async, so these will just be requests,
 # and the responses will be sent to the UI when they arrive. Probably use queues.
-def bg_thread(jsWindow):
+def bg_thread(jsWindow, queue):
     while(True):  
-        updateSMeter(jsWindow)
-        updateFreq(jsWindow)
-        updateMode(jsWindow)
+        if not queue.empty():
+            response = queue.get()
+            parts = response.split(":")
+            command = parts[0]
+            value = parts[1]
+            match command:
+                case "freq":
+                    updateFreq(jsWindow, parts[1])
+
+                case "signal_strength":
+                    slevel = parts[1]
+                    updateSMeter(jsWindow, slevel)
+
+                case _:
+                    print(f"unhandled: parts[0] == {parts[0]}")
+            
         time.sleep(0.5)
 
 # TODO: consider using a responsive layout and making the window resizeable
 window = webview.create_window(title="RigClient", url="rigClient.html", width=400, height=350, resizable=False);
 
-
-thread = threading.Thread(target=bg_thread, args=(window,))
+queue = Queue()
+thread = threading.Thread(target=bg_thread, args=(window, queue))
 thread.daemon = True
 thread.start()
 
-radioThread = threading.Thread(target=radio.sendVFORequest)
+radioThread = threading.Thread(target=radio.data_loop, args=(queue, ))
 radioThread.daemon = True
 radioThread.start()
 
-webview.start(debug=False)
+webview.start(debug=True)
--- radio.py
+++ radio.py
@@ -8,13 +8,68 @@ from datetime import datetime
 HOST = 'houseserver'  # The server's hostname or IP address
 PORT = 4532           # The port used by the server
 
-def sendVFORequest():
-    print("sendVFORequest")
+COMMAND_GET_FREQ = "|f\n"
+FREQ_RESPONSE_PREFIX = "get_freq:|Frequency: ";
+FREQ_RESPONSE_PREFIX_LEN = len(FREQ_RESPONSE_PREFIX)
+FREQ_RESPONSE_SUFFIX = "|RPRT 0"
+FREQ_RESPONSE_SUFFIX_LEN = len(FREQ_RESPONSE_SUFFIX)
+
+COMMAND_GET_SIGNAL_STRENGTH = "|l STRENGTH\n"
+SIGNAL_STRENGTH_RESPONSE_PREFIX = "get_level: STRENGTH|"
+SIGNAL_STRENGTH_RESPONSE_PREFIX_LEN = len(SIGNAL_STRENGTH_RESPONSE_PREFIX)
+SIGNAL_STRENGTH_RESPONSE_SUFFIX = "|RPRT 0"
+SIGNAL_STRENGTH_RESPONSE_SUFFIX_LEN = len(SIGNAL_STRENGTH_RESPONSE_SUFFIX)
+
+def strengthToSLevel(strengthStr):
+    strength = int(strengthStr)
+    # under-range (< -54dBm) - return S0
+    if strength < -54:
+      print(f'strength under-range: strength {strength} is < -54, returning 0 (s0)');
+      return 0;
+
+    # over-range: > 60dBm, return S9+60
+    if strength > 60:
+        print(f'signal strength over-range: strength {strength} is > 60dBm, returning 15 (s9+60)');
+        return 15 # don't go beyond full scale at S9+60    
+
+    # S0 (-54dBm) to S9 (0dBm) - scaled from 0 to 9 for the gauge
+    if strength <= 0 :
+      return (strength + 54) / 6
+
+    # S9+ : 1dBm (S9+1), up to 60dBm (S9+60), scaled to 10 to 15 for the gauge
+    if strength > 0:
+      return (strength / 10) + 9 
+
+
+def parseResponse(response):
+    if response.startswith(FREQ_RESPONSE_PREFIX):
+        val = response[FREQ_RESPONSE_PREFIX_LEN:] # e.g. 14074840|RPRT 0
+        val = val[:FREQ_RESPONSE_SUFFIX_LEN]
+        return f"freq:{val}"
+    
+    if response.startswith(SIGNAL_STRENGTH_RESPONSE_PREFIX):
+        val = response[SIGNAL_STRENGTH_RESPONSE_PREFIX_LEN:]  # e.g. '21|RPRT 0'
+        dBm = val[:-(SIGNAL_STRENGTH_RESPONSE_SUFFIX_LEN+1)] # e.g. '21'
+        # todo - convert from dBm to s-units
+        sLevel = strengthToSLevel(dBm)
+        return f"signal_strength:{round(sLevel,1)}" # sLevel is 0-15 for the gauge, dBm is the s-number label
+
+    print(f"Unhandled response: {response}")
+    return ""
+
+def sendRequest(socket, command, responseQueue):
+    socket.sendall(command.encode())
+    data = socket.recv(1024)
+    rawResponse = data.decode()
+    # print(f"raw: {rawResponse}")
+    responseValue = parseResponse(rawResponse)
+    if len(responseValue) > 0:
+        responseQueue.put(responseValue)
+
+def data_loop(responseQueue):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         while True:
-            command_to_send = "|f"
-            s.sendall(command_to_send.encode())
-            data = s.recv(1024)
-            print(f"{datetime.now().time()}: {data.decode()}")
+            sendRequest(s, COMMAND_GET_FREQ, responseQueue)
+            sendRequest(s, COMMAND_GET_SIGNAL_STRENGTH, responseQueue)
             time.sleep(1)
